3c5802125cc95a71d78b6ea3b7f570275abe9236
Nirvana-orv <imtanvikmr@gmail.com>
2026-03-02 23:31:55 +0530

    Update chat-container.tsx

--- components/chat-container.tsx
+++ components/chat-container.tsx
@@ -107,7 +107,7 @@ export function ChatContainer() {
 
   try {
     const res = await fetch(
-      "/api/chat",
+      "https://drogon-backend.vercel.app/api/chat",
       {
         method: "POST",
         headers: {
